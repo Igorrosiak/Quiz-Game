c7ac263a424986c0aa8f9286962ccf36afb13ff0
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-07-11 23:17:04 -0300

    Updating Screens

---
+++ app/src/main/java/br/senai/Conteudo.java
@@ -0,0 +1,14 @@
+package br.senai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Conteudo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conteudo);
+    }
+}
